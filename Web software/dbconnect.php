b6dc0730fb2bba2b4a7d13956d6272c721204d17
Ajadi samuel <99180312+Samajadi@users.noreply.github.com>
2022-11-02 02:30:25 +0100

    Add files via upload

---
+++ Web software/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+
+$db = mysqli_connect("localhost",  "id17130795_byker",  "Danielkomolafe00%",  "id17130795_energymeter");
+
+if(!$db)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+
+
+
+ 
