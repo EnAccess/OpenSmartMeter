d6416141ebc26eabecf9f392f2e847c1e1231580
MichelFripiat <fripiat.m@solarly.energy>
2023-10-31 16:00:59 +0100

    Reformatting with Black tool

--- firmware/test/test_openpaygo.py
+++ firmware/test/test_openpaygo.py
@@ -3,12 +3,12 @@ from firmware.test.model import Device
 
 # Device definition
 device_settings = {
-    'serial': 'OSM10000001',
-    'starting_code': 407592873,
-    'secret_key': '47a01268b629e1b027fe20c99309643f',
-    'restricted_digit_set': False,
-    'value_divider': 1,
-    'count': 0
+    "serial": "OSM10000001",
+    "starting_code": 407592873,
+    "secret_key": "47a01268b629e1b027fe20c99309643f",
+    "restricted_digit_set": False,
+    "value_divider": 1,
+    "count": 0,
 }
 # Token definition
 token_type = TokenType.ADD_TIME  # ADD_TIME, SET_TIME, DISABLE_PAYG
@@ -23,7 +23,7 @@ device.count, new_token = generate_token(
     token_type=TokenType.ADD_TIME,
     starting_code=device.starting_code,
     value_divider=device.value_divider,
-    restricted_digit_set=device.restricted_digit_set
+    restricted_digit_set=device.restricted_digit_set,
 )
 
-print('Token: ' + new_token)
+print("Token: " + new_token)
