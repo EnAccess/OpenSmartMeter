a8950b83d65669d33a8680edd053f09aec625cc8
Daniel Mohns <dmohns@users.noreply.github.com>
2023-12-20 11:52:07 +0100

    Revert "Reorder imports (#117)" (#150)
    This reverts commit 67e2c34736f4fb316f7ca69ac215391d00c420df.

--- firmware/src/credit.hpp
+++ firmware/src/credit.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 // defines
 
 // Arduino base libraries
--- firmware/src/opaygo_functions.hpp
+++ firmware/src/opaygo_functions.hpp
@@ -1,17 +1,16 @@
 #pragma once
 
-// defines
-
-// Arduino base libraries
-#include "Arduino.h"
-
-// third party libraries
-
 // OpenSmartMeter libraries
 #include "global_defines.hpp"
 #include "helpers.hpp"
 #include "mem_init.hpp"
 #include "time_management.hpp"
+
+// Arduino base libraries
+#include <inttypes.h>
+#include "Arduino.h"
+// #include <time.h>
+
 extern "C" {
 #include "opaygo_decoder.h"
 }
