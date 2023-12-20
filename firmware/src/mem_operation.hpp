6ab62741c3bac040d9e1f1b9afde0963ca8b9e7b
Daniel Mohns <dmohns@users.noreply.github.com>
2023-12-20 11:45:30 +0100

    Revert "Factory setting mode (#140)"
    This reverts commit 36472224bf68d7266c0a5ee52bdc03a524dc1279.

--- firmware/src/mem_operation.hpp
+++ firmware/src/mem_operation.hpp
@@ -29,8 +29,6 @@ uint32_t ActiveUntil = 0;
 uint32_t TokenEntryLockedUntil = 0;
 uint8_t nbDisconnections = 0;
 
-int InvalidTokenCount = 0;
-
 void LoadActivationVariables() {
   TokenCount =
       mem.readInt(TokenCount_eeprom_location);  // We load TokenCount (&
