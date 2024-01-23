6530875cc552b4cf811b6ca844e7916b5d8e23f1
Daniel Mohns <daniel.mohns@posteo.de>
2024-01-23 16:28:09 +0100

    Improvements to backend documentation

--- docs_website/docusaurus.config.js
+++ docs_website/docusaurus.config.js
@@ -197,6 +197,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ["powershell", "php"],
       },
     }),
 };
